9b6b96c75593fe6874a1a5c8732ff79fdec49da7
Sibo14 <sibzmbatha870@gmail.com>
2021-02-19 12:13:46 +0200

    2

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import "./App.css";
 import Navbar from "./Components/navbar";
 import MainGrid from "./Components/MainGrid";
 import Drawer from "./Components/Drawer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <MainGrid />
+      <Router>
+        <ApiExample />
+      </Router>
     </div>
   );
 }
