bdc821af9c946c7f649deb5c6e5c134f801445af
Sibo14 <sibzmbatha870@gmail.com>
2021-02-19 12:18:59 +0200

    api

--- src/App.js
+++ src/App.js
@@ -10,9 +10,6 @@ function App() {
     <div className="App">
       <Navbar />
       <MainGrid />
-      <Router>
-        <ApiExample />
-      </Router>
     </div>
   );
 }
---
+++ src/Components/ApiExample.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ApiExample() {
+  return (
+    <div>
+      <p>Api</p>
+    </div>
+  );
+}
+
+export default ApiExample;
